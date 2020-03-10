7ab1b1ffb9abb755c9a0693ecd463186d069e3e0
aditya109 <adikid1996@gmail.com>
2020-03-10 16:03:04 +0530

    adding backend support 10-03-2020
    Signed-off-by: aditya109 <adikid1996@gmail.com>

---
+++ chatbot-ui/src/containers/HomeContainer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class HomeContainer extends React.Component {
+ render() {
+    return (
+      <div className = "HomeContainer">
+        HomeContainer
+      </div>
+    )
+  }
+}
+
+
+
+
+export default HomeContainer;
