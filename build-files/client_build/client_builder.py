f6afc811428df85c4367ba18418863ef067c5d13
aditya109 <adikid1996@gmail.com>
2020-03-20 16:40:11 +0530

    refactored local build
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- build-files/client_build/client_builder.py
+++ build-files/client_build/client_builder.py
@@ -5,7 +5,9 @@ import helper as help_modules
 def client_trigger():
     
     aux.update_bot_repo_path(f'{aux.bot_repo_path}\\bot-ui')
+    print(aux.bot_repo_path)
     aux.change_working_path()
+    os.system("npm install")
     os.system("npm start")
 
 aux = help_modules.Helper()
